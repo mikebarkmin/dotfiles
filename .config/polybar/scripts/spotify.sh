5459f668906c3710d508bb35d7474b5cb82457a6
Mike Barkmin <mike.barkmin@uni-due.de>
2020-04-20 22:06:40 +0200

    Fix stuff

--- .config/polybar/scripts/spotify.sh
+++ .config/polybar/scripts/spotify.sh
@@ -16,8 +16,8 @@ main() {
   album=$(echo "$meta" | sed -nr '/xesam:album"/,+2s/^ +variant +string "(.*)"$/\1/p' | tail -1| sed 's/\&/\\&/g'| sed 's#\/#\\/#g')
   title=$(echo "$meta" | sed -nr '/xesam:title"/,+2s/^ +variant +string "(.*)"$/\1/p' | tail -1 | sed 's/\&/\\&/g'| sed 's#\/#\\/#g')
 
-  echo "${*:-%artist% - %title%}" | sed "s/%artist%/$artist/g;s/%title%/$title/g;s/%album%/$album/g"i | sed "s/\&/\&/g" | sed "s#\/#\/#g"
-
+  echo "${*:-%artist% - %title%}" | sed "s/%artist%/$artist/g;s/%title%/$title/g;s/%album%/$album/g"i | sed "s/\&/\&/g" | sed "s#\/#\/#g" > ~/.config/polybar/scripts/spotify_current
+  cat ~/.config/polybar/scripts/spotify_current
 }
 
 main "$@"
